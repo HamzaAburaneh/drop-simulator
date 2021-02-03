9144ced1c5f69ffdb3359a7a46bfbdbfcd893d73
mxp190009 <marshallpurdom@gmail.com>
2021-02-03 08:08:29 -0600

    initial template changes

--- src/test/java/com/DropSimulator/DropSimulatorPluginTest.java
+++ src/test/java/com/DropSimulator/DropSimulatorPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.DropSimulator;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class DropSimulatorPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(DropSimulatorPlugin.class);
 		RuneLite.main(args);
 	}
 }
